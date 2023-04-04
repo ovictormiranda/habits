870d90e05aceecef70a1133450c61f15a65f60af
Miranda <48577840+ovictormiranda@users.noreply.github.com>
2023-04-04 06:55:26 -0300

    MOBILE - Criação layout tela de hábitos/dia + ajustes de css gerais

--- mobile/src/screens/Habit.tsx
+++ mobile/src/screens/Habit.tsx
@@ -1,6 +1,10 @@
-import { View } from "react-native";
-
+import { ScrollView, View, Text } from "react-native";
 import { useRoute } from "@react-navigation/native";
+import dayjs from 'dayjs';
+
+import { BackButton } from "../components/BackButton";
+import { ProgressBar } from "../components/ProgressBar";
+import { CheckBox } from "../components/CheckBox";
 
 interface Params {
   date: string;
@@ -9,8 +13,41 @@ interface Params {
 export function Habit(){
   const route = useRoute();
   const { date } = route.params as Params;
+
+  const parsedDate = dayjs(date);
+  const dayOfWeek = parsedDate.format('dddd');
+  const dayAndMonth = parsedDate.format('DD/MM');
+
   return (
     <View className="flex bg-background px-8 pt-16">
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100}}
+      >
+        <BackButton/>
+
+        <Text className="mt-6 text-zinc-400 font-semibold text-base lowercase">
+          {dayOfWeek}
+        </Text>
+
+        <Text className="text-white font font-extrabold text-3xl">
+          {dayAndMonth}
+        </Text>
+
+        <ProgressBar progress={30}/>
+
+        <View className="mt-6">
+          <CheckBox  
+            title="Beber 2L de água"
+            checked={false}
+          />
+
+          <CheckBox  
+            title="Acordar cedo"
+            checked={true}
+          />
+        </View>
+      </ScrollView>
 
     </View>
   )
