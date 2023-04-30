8ee03c946f7ac34d33e79bd634757795e1053c3a
Miranda <48577840+ovictormiranda@users.noreply.github.com>
2023-04-30 00:36:55 -0300

    MOBILE - Integração com api, buscando informaçoes da home na api, style de cores por porcentagem, e cadastrar novo hábito

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
-import { ScrollView, View, Text, TextInput, TouchableOpacity } from "react-native";
+import { ScrollView, View, Text, TextInput, TouchableOpacity, Alert } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import colors from "tailwindcss/colors";
 
 import { BackButton } from "../components/BackButton";
 import { CheckBox } from "../components/CheckBox";
+import { api } from "../lib/axios";
 
 const availableWeekDays = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado']
 
 export function New(){
+  const [title, setTitle] = useState("");
   const [weekDays, setWeekDays] = useState<number[]>([]);
 
   function handleToggleWeekDay(weekDayIndex: number){
@@ -16,6 +18,24 @@ export function New(){
       setWeekDays(prevState => prevState.filter(weekDay => weekDay !== weekDayIndex));
     } else {
       setWeekDays(prevState => [...prevState, weekDayIndex]);
+    }
+  }
+
+  async function handleCreateNewHabit(){
+    try {
+      if (!title.trim() || weekDays.length === 0) {   //o .trim() remove os espaços na verificaçao, ou seja, se o usuario tentar criar um hábito apenas "digitando" um espaço, ele nao vai conseguir.
+        Alert.alert('Novo Hábito', 'Informe o nome do hábito e defina a periodicidade.')
+      }
+
+      await api.post('/habits', { title, weekDays });
+
+      setTitle('');
+      setWeekDays([]);
+
+      Alert.alert('Novo hábito', 'Hábito criado com sucesso! :)')
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Opsss..!','Não foi possível criar o novo hábito!')
     }
   }
 
@@ -40,6 +60,8 @@ export function New(){
           className="h-12 pl-4 rounded-lg mt-3 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-green-600"
           placeholder="Exercícios, estudar, etc..."
           placeholderTextColor={colors.zinc[400]}
+          onChangeText={setTitle}
+          value={title}
         />
 
         <Text className="font-semibold mt-4 mb-3 text-white text-base">
@@ -59,6 +81,8 @@ export function New(){
 
         <TouchableOpacity
           className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
+          activeOpacity={0.7}
+          onPress={handleCreateNewHabit}
         >
           <Feather 
             name="check"
